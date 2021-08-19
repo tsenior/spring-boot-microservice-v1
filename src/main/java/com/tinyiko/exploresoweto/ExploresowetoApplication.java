6e36316e35385ab685fc9e1d5b959a5ce06fcd6d
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-08-19 11:49:26 +0200

    springboot app initial commit

---
+++ src/main/java/com/tinyiko/exploresoweto/ExploresowetoApplication.java
@@ -0,0 +1,13 @@
+package com.tinyiko.exploresoweto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploresowetoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExploresowetoApplication.class, args);
+    }
+
+}
